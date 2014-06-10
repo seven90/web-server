ef01f20a1d3cfdd4a048c7c8553440dfac71c6f9
Seven90 <stevesurya@gmail.com>
2014-06-10 10:07:32 -0400

    Server works, various bootstrap templates

--- server.rb
+++ server.rb
@@ -15,6 +15,29 @@ loop do                                             # Server runs forever
   end
   puts lines                                        # Output the full request to stdout
 
-  client.puts(Time.now.ctime)                       # Output the current time to the client
-  client.close                                      # Disconnect from the client
+  filename = lines[0].gsub(/GET \//, '').gsub(/ HTTP.*/,'')
+
+  if File.exists?(filename)
+    client.puts "HTTP/1.1 200 OK\r\n"
+
+    if filename =~ /.css$/ 
+
+      client.puts "Content-type: text/css\r\n\r\n" 
+    elsif filename =~ /.html$/
+      client.puts "Content-type: text/html\r\n\r\n"
+    else
+      client.puts "Content-type: text/javascript\r\n\r\n"
+    end 
+    
+    client.puts File.read(filename)
+  else 
+     client.puts "HTTP/1.1 404 File Not Found\r\n"
+  end
+  # response = File.read('index.html')
+
+  # client.puts response
+  client.close 
+
+  # client.puts(Time.now.ctime)                       # Output the current time to the client
+  # client.close                                      # Disconnect from the client
 end
